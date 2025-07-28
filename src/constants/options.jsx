936aeb465a97668f8525a12a8ddf61b16a5335c6
Raushan Raj <raushanraj3107@gmal.com>
2025-07-29 01:17:40 +0530

    added shadcn and constants option

---
+++ src/constants/options.jsx
@@ -0,0 +1,63 @@
+import { ImAirplane } from "react-icons/im";
+
+export const SelectTravelesList=[
+    {
+        id:1,
+        title:'Just me',
+        desc:'A Sole traveles in exploration',
+        icon:<ImAirplane />,
+        people:'1'
+    },
+      {
+        id:2,
+        title:'A Couple',
+        desc:'two traveles in tandem',
+        icon:<ImAirplane />,
+        people:'2 people'
+    },
+      {
+        id:3,
+        title:'Family',
+        desc:'A group of fun loving adv',
+        icon:<ImAirplane />,
+        people:'3-5 people'
+    },
+      {
+        id:4,
+        title:'Group of Friends',
+        desc:'a bunch of thrill seekes',
+        icon:<ImAirplane />,
+        people:'6+ people'
+    },
+]
+
+export const SelectBudgetOptions=[
+  {
+     id:1,
+     title:'cheap',
+     desc:'stay conscious of your budget',
+    icon:<ImAirplane />,
+    
+  },
+  {
+     id:2,
+     title:'Moderate',
+     desc:'Keep cost on the average side',
+    icon:<ImAirplane />,
+    
+  },
+  {
+     id:3,
+     title:'Luxury',
+     desc:'stay conscious of your budget',
+    icon:<ImAirplane />,
+    
+  },
+  {
+     id:4,
+     title:'cheap',
+     desc:'stay conscious of your budget',
+    icon:<ImAirplane />,
+    
+  }
+]
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -2,45 +2,46 @@ import React, { useState } from 'react'
 //import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import MapboxAutocomplete from './MapboxAutocomplete';
 //const apikey = import.meta.env.VITE_GOOGLE_PLACE_API_KEY;
+import { SelectBudgetOptions } from '@/constants/options';
 
 
 function CreateTrip() {
-  const [place,setPlace]=useState();
+  const [place, setPlace] = useState();
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>Tell us Your travel preferences</h2>
       <p className='mt-3 text-gray-500 text-xl'>Just provide Some basic information ,and our Ai Smart trip planner will generate a customized itinerary based on your preferences</p>
-
-      <div className='mt-20'>
+      <div className='mt-20 flex flex-col gap-10'>
         <div>
           <h2 className='text-xl my-3 font-medium'>WHat is destination of choice</h2>
-           {/* <GooglePlacesAutocomplete
-      apiKey={apikey}
-      selectProps={{
-         value: place,
-        onChange:(v)=>{setPlace(v);console.log(v)}
+          <MapboxAutocomplete value="" onSelect={(v) => {setPlace(v); console.log(v); }} />                      
+        </div>
 
-      }}
-    /> */}
 
-    <MapboxAutocomplete
-            value=""
-            onSelect={(v) => {
-              setPlace(v);
-              console.log(v);
-            }}
-          />
-          {/* {place && (
-            <div className='mt-4 text-gray-600'>
-              <strong>Selected:</strong> {place.place_name}
-            </div>
-          )} */}
 
-   
-
+        {/* help of Shadcn */}
+        <div>
+          <h2 className='text-xl my-3 font-medium'>How Many days are you planning your trip</h2>
+          <input placeholder='ex-3' type="number" />
 
         </div>
-      </div>
+ </div>
+          <div>
+          <h2 className='text-xl my-3 font-medium'>What is your Budget?</h2>
+          <input placeholder='ex-3' type="number" />
+          <div>
+            {SelectBudgetOptions.map((item,index)=>{
+              <div key={index}>
+                <h2>{item.icon}</h2>
+
+              </div>
+
+            })}
+          </div>
+        </div>
+
+
+     
     </div>
   )
 }
